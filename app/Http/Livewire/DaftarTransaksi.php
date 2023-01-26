fb2b3ed36241fdc480bbc2d5cc4552896d542b11
devitaristy <devitaristiyanti084@gmail.com>
2023-01-26 12:01:31 +0700

    Daftar Transaksi Table

--- app/Http/Livewire/DaftarTransaksi.php
+++ app/Http/Livewire/DaftarTransaksi.php
@@ -3,11 +3,24 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Transaksi;
+use Livewire\WithPagination;
 
 class DaftarTransaksi extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
     public function render()
     {
-        return view('livewire.daftar-transaksi');
+        $transaksi = Transaksi::with('user', 'tiket')
+                    ->where('id_wisata', auth()->user()->id_wisata);
+                    
+        $transaksi = $transaksi->paginate(10);
+
+        return view('livewire.daftar-transaksi', [
+            'transaksi' => $transaksi
+        ]);
     }
 }
+
--- resources/views/livewire/daftar-transaksi.blade.php
+++ resources/views/livewire/daftar-transaksi.blade.php
@@ -1,3 +1,94 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
+<div class="container-fluid py-4">
+    
+    {{-- Tables --}}
+    {{-- @include('components.tables.table') --}}
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-4 mx-4">
+                @if (session()->has('message'))
+                    <div class=" d-flex flex-row alert alert-success mx-3 mb-0 justify-content-between" style="margin-top:30px;" x-data="{ show: true }" x-show="show">
+                        <div >
+                            {{ session('message') }}
+                        </div>
+                        <span @click=" show = false ">
+                            <i class="fa fa-times" style="font-size:12px"></i>
+                        </span>
+                    </div>
+                @endif
+                <div class="card-header pb-0">
+                    <div>
+                        <h5 class="mb-3">Daftar Transaksi Hari Ini</h5>
+                    </div>
+                </div>
+                <div class="card-body px-0 pt-0 pb-2">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        No
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        Waktu Transaksi
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Penanggung Jawab
+                                    </th>
+                                    <th class="text-left text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Jenis Tiket
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Jumlah   
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Pendapatan  
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($transaksi as $data)
+                                    <tr>
+                                        <td class="ps-4">
+                                            <p class="text-center font-weight-bold mb-0">
+                                                {{ $transaksi->firstItem() + $loop->index }}
+                                            </p>
+                                        </td>
+                                        <td class="">
+                                            <p class="text-center font-weight-bold mb-0">
+                                                {{ $data->waktu_transaksi }}
+                                            </p>
+                                        </td>
+                                        <td class="">
+                                            <p class="text-left font-weight-bold mb-0">
+                                                {{ $data->user->name ?? '-' }}
+                                            </p>
+                                        </td>
+                                        <td class="">
+                                            <p class="text-left font-weight-bold mb-0">
+                                                {{ $data->tiket->nama_tiket }}
+                                            </p>
+                                        </td>
+                                        <td class="">
+                                            <p class="text-center font-weight-bold mb-0">
+                                                {{ $data->jumlah_tiket }}
+                                            </p>
+                                        </td>
+                                        <td class="">
+                                            <p class="text-center font-weight-bold mb-0">
+                                                {{ $data->total_pendapatan }}
+                                            </p>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        <div class="p-4">
+                            {{ $transaksi->links() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
 </div>
