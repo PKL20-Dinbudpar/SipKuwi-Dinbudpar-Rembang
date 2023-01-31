8610f8750e2416563235e5538b1c790f2d360124
MKMaarif <maarif2910@students.undip.ac.id>
2023-01-31 14:16:41 +0700

    Fix Export Heading

--- app/Exports/UserExport.php
+++ app/Exports/UserExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class UserExport implements FromCollection
+class UserExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,8 +14,15 @@ class UserExport implements FromCollection
     public function collection()
     {
         return User::with('wisata')
-                    ->select('users.id', 'users.name', 'users.username', 'users.email', 'users.password', 'wisata.nama_wisata')
+                    ->select('users.id', 'users.name', 'users.username', 'users.email', 'users.pass', 'users.role', 'wisata.nama_wisata', 'hotel.nama_hotel')
                     ->leftjoin('wisata', 'users.id_wisata', '=', 'wisata.id_wisata')
+                    ->leftjoin('hotel', 'users.id_hotel', '=', 'hotel.id_hotel')
+                    ->orderBy('role', 'asc')
                     ->get();
+    }
+
+    public function headings(): array
+    {
+        return ["id", "nama", "username", "email", "password", "role", "wisata", "hotel"];
     }
 }
--- app/Exports/WisataExport.php
+++ app/Exports/WisataExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Wisata;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class WisataExport implements FromCollection
+class WisataExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -16,5 +17,10 @@ class WisataExport implements FromCollection
                     ->select('wisata.id_wisata', 'wisata.nama_wisata', 'wisata.alamat', 'kecamatan.nama_kecamatan')
                     ->leftjoin('kecamatan', 'wisata.id_kecamatan', '=', 'kecamatan.id_kecamatan')
                     ->get();
+    }
+
+    public function headings(): array
+    {
+        return ["id", "nama", "alamat", "kecamatan"];
     }
 }
